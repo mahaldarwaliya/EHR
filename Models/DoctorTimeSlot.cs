e8f81264e019ae87aecf62a3c9c7f58a9c775713
vallurummeasma-design <vallurummeasma@gmail.com>
2026-02-05 16:25:49 +0530

    first commit

---
+++ Models/DoctorTimeSlot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EHR01.Models
+{
+    public class DoctorTimeSlot
+    {
+        [Key]
+        public int SlotId { get; set; }
+
+        [Required]
+        public int DoctorId { get; set; }
+
+        [Required]
+        public DateTime SlotDate { get; set; }
+
+        [Required]
+        public TimeSpan StartTime { get; set; }
+
+        [Required]
+        public TimeSpan EndTime { get; set; }
+
+        public bool IsBooked { get; set; }
+    }
+}
