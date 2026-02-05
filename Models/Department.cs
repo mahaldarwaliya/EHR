2c082332cd93d0a693822740335bca72f2635798
kollirajeshwari368-source <kollirajeshwari368@gmail.com>
2026-02-05 16:23:40 +0530

    First Comment

---
+++ Models/Department.cs
@@ -0,0 +1,31 @@
+﻿using EHR01.Modules.Department.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HospitalEHR.Modules.Department.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? DepartmentName { get; set; }
+
+        [MaxLength(255)]
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        // Navigation Properties
+        public ICollection<DepartmentDoctors> DepartmentDoctors { get; set; }
+        public ICollection<DepartmentService> DepartmentServices { get; set; }
+        public ICollection<DepartmentRoom> DepartmentRooms { get; set; }
+    }
+}
---
+++ Models/DepartmentRoom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalEHR.Modules.Department.Models
+{
+    public class DepartmentRoom
+    {
+        [Key]
+        public int RoomId { get; set; }
+
+        [ForeignKey(nameof(Department))]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? RoomNumber { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? RoomType { get; set; } // OPD / ICU / Ward
+
+        public int Capacity { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation
+        public Department Department { get; set; }
+    }
+}
