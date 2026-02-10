b55b29ead5737d026e04c39eff52ad535a0012eb
nandini919 <mnandini672@gmail.com>
2026-02-10 11:47:21 +0530

    2nd  commit

---
+++ Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EHR01.Data;
+using EHR01.Repositories.Interfaces;
+
+namespace EHR01.Repositories.Implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly PharmacyDbContext _context;
+        private readonly DbSet<T> _table;
+
+        public GenericRepository(PharmacyDbContext context)
+        {
+            _context = context;
+            _table = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _table.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _table.FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _table.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _table.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _table.Remove(entity);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
